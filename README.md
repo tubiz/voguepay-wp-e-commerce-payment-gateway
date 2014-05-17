cb54c69addd706d80711d794eeb3117170f050a8
Tunbosun Ayinla <tayinla@gmail.com>
2014-05-17 07:19:05 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Voguepay payment gateway for [WP eCommerce](http://wordpress.org/plugins/wp-e-commerce/)
---
+++ voguepay-wp-e-commerce-payment-gateway.php
@@ -0,0 +1,11 @@
+<?php
+/*
+	Plugin Name: Voguepay WP eCommerce Payment Gateway
+	Plugin URI: http://bosun.me/voguepay-wp-ecommerce-payment-gateway
+	Description: Voguepay WP eCommerce Payment Gateway.
+	Version: 1.0.0
+	Author: Tunbosun Ayinla
+	Author URI: http://bosun.me/
+	License:  GPL-2.0+
+ 	License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
+*/
